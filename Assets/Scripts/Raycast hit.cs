b0faf6c39339377778d7f408be91dc2719f111f0
Preston Giorgianni <Preston Giorgianni>
2016-04-17 11:36:18 -0700

    vr classroom work

---
+++ Assets/Scripts/Raycast hit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycasthit : MonoBehaviour {
+
+// Use this for initialization
+void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        var cameraCenter = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, camera.nearClipPlane));
+        if (Physics.Raycast(cameraCenter, this.transform.forward, out hit, 1000))
+        {
+            var obj = hit.transform.gameObject;
+        }
+    }
+}
