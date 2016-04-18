6486de97e667df9c94138598296ca9c023d70e22
Henry Garcia <hgarc014@ucr.edu>
2016-04-18 10:38:33 -0700

    added Bundle Identifier for android build

--- Assets/Scripts/Raycast hit.cs
+++ Assets/Scripts/Raycast hit.cs
@@ -11,7 +11,7 @@ void Start () {
 	// Update is called once per frame
 	void Update () {
         RaycastHit hit;
-        var cameraCenter = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, camera.nearClipPlane));
+        var cameraCenter = GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, GetComponent<Camera>().nearClipPlane));
         if (Physics.Raycast(cameraCenter, this.transform.forward, out hit, 1000))
         {
             var obj = hit.transform.gameObject;
