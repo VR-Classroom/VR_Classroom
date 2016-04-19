54726a3fccaa55a94b966461f835d71ca0fdbe70
Preston Giorgianni <Preston Giorgianni>
2016-04-18 17:02:07 -0700

    material orginization + clickable doors

---
+++ Assets/Scripts/Raycast_Hit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycast_Hit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        //debug
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 20;
+        Debug.DrawRay(transform.position, (forward), Color.green);
+
+        if (Physics.Raycast(transform.position, (forward), out hit))
+        {
+            hit.transform.SendMessage("HitByRaycast", gameObject,
+                              SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
