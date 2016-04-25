0117184f435f8e53f9a46baf41a2584a1d079a52
Henry Garcia <hgarc014@ucr.edu>
2016-04-24 20:43:22 -0700

    added photon for networking solution

---
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    const string VERSION = "v0.0.1";
+    public string roomName = "TEST";
+    public string prefabName = "User";
+    public Transform[] spawnPoints;
+
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+	}
+
+    void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 10 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        //TODO: figure out how to spawn players in order not randomly
+        Transform t = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        PhotonNetwork.Instantiate(prefabName, t.position,t.rotation,0);
+    }
+	
+}
