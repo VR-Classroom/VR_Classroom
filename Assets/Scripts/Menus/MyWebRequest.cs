bcf9dcf33e9f3f24c027ffd3e7e99019818b3ed4
Henry Garcia <hgarc014@ucr.edu>
2016-05-01 13:51:47 -0700

    added a login screen

---
+++ Assets/Scripts/Menus/MyWebRequest.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyWebRequest : MonoBehaviour
+{
+    private static MyWebRequest m_Instance = null;
+    public static MyWebRequest Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = (MyWebRequest)FindObjectOfType(typeof(MyWebRequest));
+                if (m_Instance == null)
+                    m_Instance = (new GameObject("WebRequest")).AddComponent<MyWebRequest>();
+                DontDestroyOnLoad(m_Instance.gameObject);
+            }
+            return m_Instance;
+        }
+    }
+    public static Coroutine Get(string aURL, System.Action<bool, string> aCallback)
+    {
+        return Instance.StartCoroutine(_GetRequest(aURL, aCallback));
+    }
+    public static Coroutine Post(string aURL, WWWForm aForm, System.Action<bool, string> aCallback)
+    {
+        return Instance.StartCoroutine(_PostRequest(aURL, aForm, aCallback));
+    }
+
+    private static IEnumerator _GetRequest(string aURL, System.Action<bool, string> aCallback)
+    {
+        WWW request = new WWW(aURL);
+        yield return request;
+        if (string.IsNullOrEmpty(request.error))
+        {
+            if (aCallback != null)
+                aCallback(true, request.text);
+        }
+        else
+        {
+            if (aCallback != null)
+                aCallback(false, request.error);
+        }
+    }
+    private static IEnumerator _PostRequest(string aURL, WWWForm aForm, System.Action<bool, string> aCallback)
+    {
+        WWW request = new WWW(aURL, aForm);
+        yield return request;
+        if (string.IsNullOrEmpty(request.error))
+        {
+            if (aCallback != null)
+                aCallback(true, request.text);
+        }
+        else
+        {
+            if (aCallback != null)
+                aCallback(false, request.error);
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/test.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    string highscore_url = "http://52.38.66.127/scripts/getUser.php";
+    string playName = "Player 1";
+    int score = -1;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+
+
+        // Create a form object for sending high score data to the server
+        WWWForm form = new WWWForm();
+        form.AddField("EMAIL", "hgarc014@ucr.edu");
+        form.AddField("PASS", "1234");
+        
+        WWW download = new WWW(highscore_url, form);
+
+        // Wait until the download is done
+        yield return download;
+
+        if (!string.IsNullOrEmpty(download.error))
+        {
+            print("Error downloading: " + download.error);
+        }
+        else {
+            string data = download.text;
+            if (data == null || data.Trim() == "")
+            {
+                Debug.Log("Invalid input");
+            }
+            else {
+                string[] rows = data.Split(';');
+                Debug.Log(GetValue(rows[0], "email"));
+                Debug.Log(download.text);
+            }
+        }
+    }
+
+    string GetValue(string row, string name)
+    {
+        string value = row.Substring(row.IndexOf(name+":") + name.Length+1);
+        if (value.Contains("|"))
+            value = value.Remove(value.IndexOf("|"));
+        return value;
+    }
+}
+
+
