31e316fb524092f4dd412cbef09f469aa60c79eb
Henry Garcia <hgarc014@ucr.edu>
2016-05-01 19:25:03 -0700

    might have added head movement

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+
+    public Camera c;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.rotation = Quaternion.LookRotation(c.transform.forward, -c.transform.up);
+        //transform.Rotate(0, Time.deltaTime * 10, 0, Space.World);
+    }
+}
